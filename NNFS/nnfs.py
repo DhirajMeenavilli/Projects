0ce7d6aad50764b3a2532902395cdad4a318b157
Dhiraj Meenavilli <meenavillid@gmail.com>
2023-09-05 19:25:39 -0600

    First 2 node neural network

--- NNFS/nnfs.py
+++ NNFS/nnfs.py
@@ -14,11 +14,20 @@ class neuron:
         total = 0
         for i in range(len(inputs)):
             total += self.compute(inputs[i],weights[i])
-        return total
+        return total # Strictly linear function can be turned into ReLU later
 
 inputs = [3, 5, 7]
-weights = [1,2,1]
+weights = [[1,2,1],[1,1,2]]
 
-node1 = neuron()
+# node1 = neuron()
+# node2 = neuron()
 
-print(node1.output(inputs, weights))
+firstLayer = []
+firstLayerSize = 2
+
+for i in range(firstLayerSize):
+    firstLayer.append(neuron())
+    print(firstLayer[i].output(inputs, weights[i]))
+
+# print(firstLayer[0].output(inputs, weights[0]))
+# print(firstLayer[1].output(inputs, weights[1]))
