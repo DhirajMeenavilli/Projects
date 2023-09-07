9d76237ae280e4a5fda45db523c563bc9e65107d
Dhiraj Meenavilli <meenavillid@gmail.com>
2023-09-06 20:54:25 -0600

    Class can calculate loss now I just need to do backprop and then it will be done

--- NNFS/nnfs.py
+++ NNFS/nnfs.py
@@ -3,23 +3,31 @@ import numpy as np
 import matplotlib
 
 class architechture:
-    def __init__(self, inputs, weights, curves):
-        # self.layers = [input] Have to decide how to use this properly but that's a later question
+    def __init__(self, inputs, output, weights, curves, loss):
         self.inputs = inputs
+        self.output = output
         self.weights = weights
         self.curves = curves
+        self.loss = loss
 
     def generate(self):
         #Based on a 3D array of weights it should generate the amount of layers needed as well as the number of nodes in each layer
         layeroutputs = self.inputs
+        results = []
         for i in range(len(self.weights)):
-            results = []
+            results.append([])
             for j in range(len(self.weights[i])):
-                results.append(neuron(self.curves[i]).output(layeroutputs,self.weights[i][j]))
-            layeroutputs = results
-
+                results[i].append(neuron(self.curves[i]).output(layeroutputs,self.weights[i][j]))
+            layeroutputs = results[i]
+        self.modelOutput = layeroutputs
         return layeroutputs
 
+    #Now I have to be able to backpropogate to alter the weights and bias of each neuron.
+    def error(self):
+        if self.loss == "MSE":
+            modelError = (sum(self.modelOutput) - self.output)**2 
+        
+        return modelError
 
 class neuron:
     def __init__(self, curve):
@@ -54,44 +62,46 @@ class neuron:
         return total # Strictly linear function can be turned into ReLU later
 
 inputs = [3, 5, 7, 4, 5, 2] # Each input is a node
+trueOutput = 164.075
+
 firstLayerWeights = [[1,2,1,0,1,1],[1,1,2,0,1,1],[2,1,1,0,1,1], [0.1,0.1,0.1,0,1,1], [1,0.5,2,0,1,1]] # Each node is fully connected
 
 # node1 = neuron()
 # node2 = neuron()
 
-firstLayer = []
-firstLayerSize = len(firstLayerWeights)
+# firstLayer = []
+# firstLayerSize = len(firstLayerWeights)
 
-firstLayerOutputs = []
-for i in range(firstLayerSize): # Thus 5 nodes are created
-    firstLayer.append(neuron("ReLU"))
-    firstLayerOutputs.append(firstLayer[i].output(inputs, firstLayerWeights[i]))
+# firstLayerOutputs = []
+# for i in range(firstLayerSize): # Thus 5 nodes are created
+#     firstLayer.append(neuron("Linear"))
+#     firstLayerOutputs.append(firstLayer[i].output(inputs, firstLayerWeights[i]))
 
 # print(firstLayerOutputs)
 
 # print(firstLayer[0].output(inputs, weights[0]))
 # print(firstLayer[1].output(inputs, weights[1]))
-secondLayer = []
+# secondLayer = []
 
 secondLayerWeights = [[0.1,0.1,0.1,1,0.1], [0.1,0.5,0.5,0.5, 1], [0.15,0.1,0.5,1,0.1], [0.15,0.1,0.1,1,0.15]]
-secondLayerSize = len(secondLayerWeights)
+# secondLayerSize = len(secondLayerWeights)
 
-secondLayerOutputs = []
-for i in range(secondLayerSize): #4 nodes
-    secondLayer.append(neuron("Sigmoid"))
-    secondLayerOutputs.append(secondLayer[i].output(firstLayerOutputs, secondLayerWeights[i]))
+# secondLayerOutputs = []
+# for i in range(secondLayerSize): #4 nodes
+#     secondLayer.append(neuron("Linear"))
+#     secondLayerOutputs.append(secondLayer[i].output(firstLayerOutputs, secondLayerWeights[i]))
 
 # print(secondLayerOutputs)
 
-outputLayer = []
+# outputLayer = []
 
 outputLayerWeights = [[1,2,0,1]] # 1 output node
-outputLayerSize = len(outputLayerWeights)
+# outputLayerSize = len(outputLayerWeights)
 
-output = []
-for i in range(outputLayerSize):
-    outputLayer.append(neuron("Linear"))
-    output.append(outputLayer[i].output(secondLayerOutputs, outputLayerWeights[i]))
+# output = []
+# for i in range(outputLayerSize):
+#     outputLayer.append(neuron("Linear"))
+#     output.append(outputLayer[i].output(secondLayerOutputs, outputLayerWeights[i]))
 
 # print(output)
 
@@ -103,11 +113,13 @@ for i in range(outputLayerSize):
 
 weights = [firstLayerWeights, secondLayerWeights, outputLayerWeights]
 
-curves = ["ReLU", "Sigmoid", "Linear"]
+curves = ["Linear", "Linear", "Linear"]
 
-neuralNet = architechture(inputs,weights, curves)
+loss = "MSE"
+
+neuralNet = architechture(inputs,trueOutput,weights,curves,loss)
 architechtureOutput = neuralNet.generate()
-
+print(neuralNet.error())
 # print(architechtureOutput)
 
 
