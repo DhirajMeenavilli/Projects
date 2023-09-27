5d3eb23526518be290990d1779f8c2cb8d30132a
Dhiraj Meenavilli <meenavillid@gmail.com>
2023-09-26 22:14:49 -0600

    Realised even if I can't commit code, I can always commit my trial for the day and the notes that come with it.

---
+++ MatplotLib/firstGraph.py
@@ -0,0 +1,3 @@
+# Dayt 1: I can't even chart a basic graph, it makes me feel very silly. I struggled with even writing anything because I couldn't even import Seaborn. I'll struggle with it more tommorow, and if I can't I'll just use matplotlib and go super super basic, just dummy data and like a bar plot or dot plot or something.
+
+
