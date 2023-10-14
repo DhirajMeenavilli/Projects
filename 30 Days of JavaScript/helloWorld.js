90a1235b3caab493281fb15e6c2cac277097e057
Dhiraj Meenavilli <meenavillid@gmail.com>
2023-10-14 13:05:54 -0600

    Created a function to return a function that says hello world.

--- 30 Days of JavaScript/helloWorld.js
+++ 30 Days of JavaScript/helloWorld.js
@@ -5,3 +5,23 @@ function HelloWorld() {
 }
 
 console.log(HelloWorld())
+
+
+// From LC
+
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+
+        return 'Hello World';
+        
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
