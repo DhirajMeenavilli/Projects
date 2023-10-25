b7acba48ae19f3216bb117c17e0bc793beb8a4e8
DhirajMeenavilli <meenavillid@gmail.com>
2023-10-24 22:25:36 -0600

    Learned how to use the Pie feature of matplotlib, a bit better.

--- Would You Have Survived The Titanic/univariateAnalysis.py
+++ Would You Have Survived The Titanic/univariateAnalysis.py
@@ -17,4 +17,18 @@ Test_data = pd.read_csv('Projects/Would You Have Survived The Titanic/Models Uns
 sns.countplot(Test_data,x='Survived')
 plt.show() # Normally this would let us ensure that we have the labels well distributed but the Kaggle people didn't give you a way to validate your training which means you get effectively one shot and no feedback which is nonsense, but the point stands and is cool.
 """
+# 2. Pie Charting
 
+#colors
+colors = ['#ff9999','#66b3ff','#99ff99','#ffcc99']
+#explsion
+explode = (0.05,0.05,0.05)
+
+plt.pie(Titanic_Dataset['Pclass'], colors = colors)
+#draw circle
+centre_circle = plt.Circle((0,0),0.70,fc='white')
+fig = plt.gcf()
+fig.gca().add_artist(centre_circle)
+# Equal aspect ratio ensures that pie is drawn as a circle
+plt.tight_layout()
+plt.show()
