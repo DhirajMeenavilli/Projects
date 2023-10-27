e970d5e94cf2adaeed0feabbed2406393e0d5596
DhirajMeenavilli <meenavillid@gmail.com>
2023-10-27 07:51:24 -0600

    Finished up the multivariate part of the learning, now on to encoding, and modeling.

--- Would You Have Survived The Titanic/multivariateAnalysis.py
+++ Would You Have Survived The Titanic/multivariateAnalysis.py
@@ -20,8 +20,27 @@ plt.show()
 sns.boxplot(x=Titanic_Dataset['Sex'], y=Titanic_Dataset['Age'], hue=Titanic_Dataset['Survived']) # Iluminates if between Men and women there was a large disparity in something like age of survivor/deceased by gender 
 plt.show()
 
-#4. Dist Plot (Numerical vs. Catergorical):
+#4. Dist Plot (Numerical vs. Catergorical): Very useful to polot conditional distributions, and compare it against other conditional distributions to see how much information you can get out of it. 
 
-#5. Heat Map (Catergorical vs. Catergorical):
+sns.distplot(Titanic_Dataset[Titanic_Dataset['Survived']==0]['Age']) # If you type in hist = False you get just the function curve.
+sns.distplot(Titanic_Dataset[Titanic_Dataset['Survived']==1]['Age'])
+plt.show()
 
-#6. Cluster Map (Catergorical vs Catergorical):
+#5. Heat Map (Catergorical vs. Catergorical): Quick and dirty way to get a sense of how the counts are looking against one another, like where is it strongly concentrated, where is it not etc.
+
+sns.heatmap(pd.crosstab(Titanic_Dataset['Pclass'],Titanic_Dataset['Survived'])) # A cross tab is just a set of counts at each meeting point, so in this case 0 against 1, 0 against 2, 0 against 3, and then same 1 against 1 etc. where the first value is survived and the second is Pclass.
+plt.show()
+
+#6. Cluster Map (Catergorical vs Catergorical): Looks cool, not sure what it's use is.
+
+sns.clustermap(pd.crosstab(Titanic_Dataset['Pclass'],Titanic_Dataset['Survived']))
+plt.show()
+
+#7. Pair Plot: Shows every pairing against one another so it could help with formulation of where encoding may need to change, bar that, possibly where a relationship may just be noise, or where the approach needs to be more precise to get an idea of if the hypothesis is valid or not.
+
+sns.pairplot(Titanic_Dataset, hue = 'Survived')
+plt.show()
+
+#8. Line Plot (Numerical vs Numerical): Can help to spot encoding issues or noise in numerical relationships.
+sns.lineplot(x = Titanic_Dataset['Age'],y = Titanic_Dataset['Fare'])
+plt.show()
