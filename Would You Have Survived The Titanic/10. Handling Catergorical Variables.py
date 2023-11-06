5067f9ba4d8a9af28df3db3ddc76072e1db5492a
DhirajMeenavilli <meenavillid@gmail.com>
2023-11-05 23:16:36 -0700

    One more down.

--- Would You Have Survived The Titanic/10. Handling Catergorical Variables.py
+++ Would You Have Survived The Titanic/10. Handling Catergorical Variables.py
@@ -1,14 +1,105 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+
+def score_dataset(X_train, X_valid, y_train, y_valid):
+    model = RandomForestRegressor(n_estimators=100, random_state=0)
+    model.fit(X_train, y_train)
+    preds = model.predict(X_valid)
+    return mean_absolute_error(y_valid, preds)
+
 
 Titanic_train = pd.read_csv('Projects/Would You Have Survived The Titanic/Models Training Passengers.csv')
 
-features = ['PassengerId','Pclass','Age','SibSp','Parch','Fare','Cabin','Sex']
-
-X = Titanic_train[features]
 y = Titanic_train.Survived
+X = Titanic_train.drop(['Survived'], axis=1)
 
+X_train_full, X_valid_full, y_train, y_valid = train_test_split(X, y, train_size=0.8, test_size=0.2, random_state=0)
+
+
+cols_with_missing = [col for col in X_train_full.columns if X_train_full[col].isnull().any()] 
+
+X_train_full.drop(cols_with_missing, axis=1, inplace=True) # This is just following the drop out columns with null values so basically Cabin Fare, and stuff
+X_valid_full.drop(cols_with_missing, axis=1, inplace=True)
+
+# "Cardinality" means the number of unique values in a column
+
+# Select categorical columns with relatively low cardinality (convenient but arbitrary)
+
+low_cardinality_cols = [cname for cname in X_train_full.columns if X_train_full[cname].nunique() < 10 and X_train_full[cname].dtype == "object"]
+
+# Select numerical columns
+
+numerical_cols = [cname for cname in X_train_full.columns if X_train_full[cname].dtype in ['int64', 'float64']]
+
+# Keep selected columns only
+
+my_cols = low_cardinality_cols + numerical_cols
+X_train = X_train_full[my_cols].copy()
+X_valid = X_valid_full[my_cols].copy()
+
+# print(X_train.head())
+
+# Get list of categorical variables
+s = (X_train.dtypes == 'object')
+object_cols = list(s[s].index)
+
+# print("Categorical variables:")
+# print(object_cols)
+# print()
 # 1. Drop Catergorical Variables: The Issue with this is obvious in that it drops possibly very useful information without good reason
 
+drop_X_train = X_train.select_dtypes(exclude=['object'])
+drop_X_valid = X_valid.select_dtypes(exclude=['object'])
+
+print(drop_X_train.head())
+print()
+print("MAE from Approach 1 (Drop categorical variables):",score_dataset(drop_X_train, drop_X_valid, y_train, y_valid))
+print()
 # 2. Ordinal Encoding (Assigning each unique value a unique interger): This can be useful but simmilar catergorical values get transformed to very different values and information is possibly lost, so can be tuned for better encoding. 
 
+from sklearn.preprocessing import OrdinalEncoder
+
+label_X_train = X_train.copy()
+label_X_valid = X_valid.copy()
+
+# Apply ordinal encoder to each column with categorical data
+
+ordinal_encoder = OrdinalEncoder()
+
+label_X_train[object_cols] = ordinal_encoder.fit_transform(X_train[object_cols])
+label_X_valid[object_cols] = ordinal_encoder.transform(X_valid[object_cols])
+
+print(label_X_train.head())
+print()
+print("MAE from Approach 2 (Drop categorical variables):",score_dataset(label_X_train, label_X_valid, y_train, y_valid))
+print()
 # 3. One Hot Encoding: Creating n columns one for each of the n unique values in a catergorical column and binarying each observation to each with either 1 or 0 for ids this unique value or not this unique value/ 
+
+from sklearn.preprocessing import OneHotEncoder
+
+# Apply one-hot encoder to each column with categorical data
+OH_encoder = OneHotEncoder(handle_unknown='ignore', sparse=False)
+OH_cols_train = pd.DataFrame(OH_encoder.fit_transform(X_train[object_cols]))
+OH_cols_valid = pd.DataFrame(OH_encoder.transform(X_valid[object_cols]))
+
+# One-hot encoding removed index; put it back
+OH_cols_train.index = X_train.index
+OH_cols_valid.index = X_valid.index
+
+# Remove categorical columns (will replace with one-hot encoding)
+num_X_train = X_train.drop(object_cols, axis=1)
+num_X_valid = X_valid.drop(object_cols, axis=1)
+
+# Add one-hot encoded columns to numerical features
+OH_X_train = pd.concat([num_X_train, OH_cols_train], axis=1)
+OH_X_valid = pd.concat([num_X_valid, OH_cols_valid], axis=1)
+
+# Ensure all columns have string type
+OH_X_train.columns = OH_X_train.columns.astype(str)
+OH_X_valid.columns = OH_X_valid.columns.astype(str)
+
+print(OH_X_train.head())
+print()
+print("MAE from Approach 3 (Drop categorical variables):",score_dataset(OH_X_train, OH_X_valid, y_train, y_valid)) # One hot encoding generally works best and it did in this case, very cool.
