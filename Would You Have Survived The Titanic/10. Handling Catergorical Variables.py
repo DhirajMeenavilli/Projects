48245406ef95cfeed9f6c0f669d20905adf91a72
DhirajMeenavilli <meenavillid@gmail.com>
2023-11-05 23:34:48 -0700

    Further steps and refinement in the process of manipulating data.

--- Would You Have Survived The Titanic/10. Handling Catergorical Variables.py
+++ Would You Have Survived The Titanic/10. Handling Catergorical Variables.py
@@ -59,6 +59,27 @@ print("MAE from Approach 1 (Drop categorical variables):",score_dataset(drop_X_t
 print()
 # 2. Ordinal Encoding (Assigning each unique value a unique interger): This can be useful but simmilar catergorical values get transformed to very different values and information is possibly lost, so can be tuned for better encoding. 
 
+# Categorical columns in the training data
+object_cols = [col for col in X_train.columns if X_train[col].dtype == "object"]
+
+# Columns that can be safely ordinal encoded
+good_label_cols = [col for col in object_cols if 
+                   set(X_valid[col]).issubset(set(X_train[col]))]
+        
+# Problematic columns that will be dropped from the dataset
+bad_label_cols = list(set(object_cols)-set(good_label_cols))
+        
+print('Categorical columns that will be ordinal encoded:', good_label_cols)
+print('Categorical columns that will be dropped from the dataset:', bad_label_cols)
+print()
+
+# Get number of unique entries in each column with categorical data
+object_nunique = list(map(lambda col: X_train[col].nunique(), object_cols))
+d = dict(zip(object_cols, object_nunique))
+
+# Print number of unique entries by column, in ascending order
+print(sorted(d.items(), key=lambda x: x[1]))
+
 from sklearn.preprocessing import OrdinalEncoder
 
 label_X_train = X_train.copy()
