f7bbc20a673eda835ed23e803d516186e4f37858
DhirajMeenavilli <48364307+DhirajMeenavilli@users.noreply.github.com>
2023-12-17 01:42:43 +0000

    3 cheers for the smallest step forward.

---
+++ Personal Pieces/Homo Economicus/Outline.py
@@ -0,0 +1,5 @@
+# If RL is effectively homo economicus, a perfect machine of rartionality weighing risk and reward 
+# would they even behave like Homo Economicus even under the stock market of BSM world, just under the constraint they don't know the future.
+# Like if options exist and are rationally priced, etc. so on and so on. Would they actually behave differently and create distortions?
+
+# Could rope in the Primer library, and just go very basic.
