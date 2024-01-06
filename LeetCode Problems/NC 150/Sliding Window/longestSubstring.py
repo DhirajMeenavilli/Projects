a6b7cc79d74a8fde3bb8e29c3b3a6327dab20cb1
DhirajMeenavilli <48364307+DhirajMeenavilli@users.noreply.github.com>
2024-01-06 05:21:44 +0000

    I think I have sliding window figured out but I'll keep working on it.

---
+++ LeetCode Problems/NC 150/Sliding Window/longestSubstring.py
@@ -0,0 +1,36 @@
+arr = list(s)
+chars = []
+l = 0
+r = 1
+maxLen = 0
+
+if len(arr) == 0:
+    print(0)
+    r = len(arr)
+
+chars.append(arr[0])
+
+while r < len(arr):
+
+    if arr[r] in chars:
+        if maxLen < len(chars):
+            maxLen = len(chars)
+        l = l + 1
+        r = l + 1
+        chars = []
+        chars.append(arr[l])
+    
+    if r < len(arr):
+        if arr[r] not in chars:
+            chars.append(arr[r])
+    
+    else:
+        print(maxLen)
+        break
+        
+    r+=1
+
+if maxLen < len(chars):
+    maxLen = len(chars)
+
+print(maxLen)
---
+++ LeetCode Problems/NC 150/Sliding Window/z_info.md
@@ -0,0 +1,1 @@
+The trick here appears to be simply intialising l = 0, r = 1, then moving r until a given condition is met at which time l is moved either by 1 or to r and r reintialized next to l.
