65a7b196fb3b70cb62be495d18da968dda221942
DhirajMeenavilli <meenavillid@gmail.com>
2024-03-13 08:34:31 +0000

    Humble begginings

---
+++ The Garage/Something Exciting/in3To5YearsTime.py
@@ -0,0 +1,8 @@
+import torch
+import torch.nn as nn
+
+"""
+We'll construct a simple linear model, and it'll predict a price for various companies, based on their fundamentals (nnormalized) and 
+we'll see if we can tune it with more data, more feature engineering, more stats, a better system, architecure, various hyperparameters and 
+then at the end of this project I can conclusively rule out the idea of statistical value investing being anything remotely real.
+"""
